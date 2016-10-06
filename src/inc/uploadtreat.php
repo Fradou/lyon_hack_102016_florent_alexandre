a4048efdec8704ac7c78f795f1e0b08d2e8910d6
Alexandre FRADET <a_fradet@yahoo.fr>
2016-10-06 18:55:12 +0200

    Put some styling and text in header. Activate css script in link. Add config and config distrib files, one is blocked by gitignore.

--- src/inc/uploadtreat.php
+++ src/inc/uploadtreat.php
@@ -5,3 +5,57 @@
  * Date: 06/10/16
  * Time: 17:53
  */
+
+include 'config.php';
+
+function getConnect()
+{
+    $user = "root";
+    $pass = "Fradou!20?07";
+    $host = "localhost";
+    $db = "hack";
+
+// Connecte à Mysql
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: ".mysqli_connect_error();
+        die();
+    }
+    return $conn;
+}
+
+$name = $_POST["name"];
+$img_url = $_POST["img_url"];
+
+// Création requete sql
+$req = "INSERT INTO hackaton (name, img_path, vote, poll_nb) values ('$name', '$img_url', False, 0)";
+
+// Envoie requete sql
+$res = getConnect()->query($req) or die('Erreur SQL!'.$req.'<br>'.mysqlerror());
+
+// Retour utilisateur
+
+
+
+
+// Requete 2 : recup tableau actuel
+$req2 = "SELECT * FROM hackaton";
+
+// Envoi requete 2
+$res2 = getConnect()->query($req2) or die('Erreur SQL 2!'.$req2.'<br>'.mysqlerror());
+
+// Recuperation sous forme de row
+$row = mysqli_fetch_row($res2);
+print_r($row);
+while ($row = $res2 -> fetch_assoc()) {
+    echo $row['first_name'] . " " . $row['last_name'] . " " . $row['email'] . " " . $row['password'] . '<br>';
+};
+
+
+
+// Cloture connexion
+mysqli_close($conn);
+
+
+?>
